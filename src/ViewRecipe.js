6df94500addb2432d1d95bf25548422614f7e2c2
Michael Hadley <michael.hadley@liferay.com>
2017-01-27 15:03:27 -0800

    Add step counter, disable buttons

--- src/ViewRecipe.js
+++ src/ViewRecipe.js
@@ -1,8 +1,9 @@
 import Component, {Config} from 'metal-jsx';
 import {bindAll} from 'lodash';
 import {connect} from 'metal-redux';
-import VoiceControl from './lib/VoiceControl';
 
+import Button from './Button';
+import VoiceControl from './lib/VoiceControl';
 import {nextStep, prevStep} from './actions/recipes';
 
 class ViewRecipe extends Component {
@@ -27,24 +28,31 @@ class ViewRecipe extends Component {
 	render() {
 		const recipe = this.props.recipe.toJS();
 
+		const stepCount = recipe.steps.length;
+
+		const {currentStep} = recipe;
+
 		return (
 			<div class="view-container">
 				{!recipe &&
-					<h2>Recipe not found!</h2>
+						<h2>Recipe not found!</h2>
 				}
 
 				{recipe &&
-					<div>
-						<h2>Title: {recipe.title}</h2>
-						<h2>description: {recipe.description}</h2>
+						<div>
+							<h2>Title: {recipe.title}</h2>
+							<h2>description: {recipe.description}</h2>
 
-						<p>{recipe.steps[recipe.currentStep]}</p>
+							<p>{recipe.steps[recipe.currentStep]}</p>
 
-						<div class="btn-group" role="group" aria-label="...">
-							  <button onClick={this.handlePrev} type="button" class="btn btn-default">Prev</button>
-							  <button onClick={this.handleNext} type="button" class="btn btn-default">Next</button>
+							<div class="btn-group" role="group" aria-label="...">
+								<Button disabled={currentStep === 0} onClick={this.handlePrev}>Prev</Button>
+								<Button disabled={currentStep === stepCount - 1} onClick={this.handleNext}>Next</Button>
+
+							</div>
+
+							<div>{`Step ${currentStep + 1} of ${recipe.steps.length}`}</div>
 						</div>
-					</div>
 				}
 			</div>
 		)
